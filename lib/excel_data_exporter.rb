6a73d674a0ba33a674b9a994faf3c7e2961b19fb
Taichi AKIYAMA <pcsa3uto@s.okayama-u.ac.jp>
2026-06-29 14:37:54 +0900

    feat: add excel data exporter

---
+++ lib/excel_data_exporter.rb
@@ -0,0 +1,20 @@
+require 'fileutils'
+require 'rubyXL'
+
+class ExcelDataExporter
+  OUTPUT_DIRECTORY = File.expand_path('../output', __dir__)
+
+  def export(workbook, file_name)
+    unless workbook.is_a?(RubyXL::Workbook)
+      raise TypeError, 'workbook must be a RubyXL::Workbook.'
+    end
+
+    unless file_name.is_a?(String)
+      raise TypeError, 'file_name must be a String.'
+    end
+
+    FileUtils.mkdir_p(OUTPUT_DIRECTORY)
+
+    workbook.write(File.join(OUTPUT_DIRECTORY, file_name))
+  end
+end
